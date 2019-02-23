4a6f3d2a228108cf719d984fabb7c83748465a9c
Accountantم <47880670+accountantM@users.noreply.github.com>
2019-02-23 02:38:42 +0200

    format code by adding <?php

--- CurlMultiTimeoutBugTester.class.php
+++ CurlMultiTimeoutBugTester.class.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * testing the curlmulti interface with many public proxies in the same time
  * cause cURL to time out for some requests with total_time info that is less that the CURLOPT_CONNECTTIMEOUT option
